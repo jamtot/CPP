81c61019438f9f94578124ba180d8ddc0e046eb8
Jonathan Morris <emailforjmorris@gmail.com>
2020-01-01 15:07:15 +1000

    GLUT is up and running, took a little while as Linux isn't as straight forward as Windows.

--- Projects/Snake/src/main.cpp
+++ Projects/Snake/src/main.cpp
@@ -1,5 +1,16 @@
+#include <GL/glut.h>
 #include <iostream>
 
-int main() {
-    std::cout << "Hello Easy C++ project!" << std::endl;
+using namespace std;
+
+int main (int argc, char **argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(800, 600);
+	//Create the window
+	glutCreateWindow("My new window");
+	glutMainLoop();
+    
+    return 0;
 }
